aaaaf71060127854edac742c76a0aad2da86167f
Kasper <35449118+KasperE1@users.noreply.github.com>
2018-09-21 09:16:55 +0200

    Changed to esx_phone3 and more

--- server.lua
+++ server.lua
@@ -2,17 +2,17 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-TriggerEvent('esx_phone:registerNumber', 'darknet', "Dark Net", true, false, true, true)
+TriggerEvent('esx_phone3:registerNumber', 'darknet', "Dark Net", true, false, true, true)
 
-RegisterServerEvent('esx_phone:reload')
-AddEventHandler('esx_phone:reload', function(phoneNumber)
+RegisterServerEvent('esx_phone3:reload')
+AddEventHandler('esx_phone3:reload', function(phoneNumber)
 
 	local _source  = source
 	local xPlayer  = ESX.GetPlayerFromId(_source)
 	local darkPhone = xPlayer.getInventoryItem('black_chip')
 
 	if darkPhone.count > 0 then
-		if xPlayer.job.name ~= 'police' then
+		if xPlayer.job.name ~= 'fib' then
 			TriggerEvent('esx_phone:addSource', 'darknet', _source)
 		end
 	end
